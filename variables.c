5f4653b13c21006b1c6fb9523878b3015de79204
kyasaram sindhuja <kyasaramsindhuja2008@gmail.com>
2026-08-19 22:35:00 +0530

    Create variables.c

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int age=18;
+  float marks=95;
+  char grade='A';
+  printf("age:%d\n,age);
+  printf("marks:%.1f\n",marks);
+  printf("grade:%c\n",grade);
+  return 0;
+}
